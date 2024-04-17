c56f31cab487f31998b23e2eb192cbfd66299f3e
biankaszendi <biankaszendi1@gmail.com>
2024-04-17 16:27:13 +0200

    Update README.md

---
+++ WebApplication1/WebApplication1/Controllers/CommentsController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlogExplorer.Data;
+using WebApplication1.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CommentsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comments>>> GetComments()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comments>> GetComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return comment;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComment(int id, Comments comment)
+        {
+            if (id != comment.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Comments>> PostComment(Comments comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComment", new { id = comment.id }, comment);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.id == id);
+        }
+    }
+}
